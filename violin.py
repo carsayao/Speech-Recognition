56c00f59311b39b363730b8f64e9d1e8d0f00464
Gabriel Miller <Miller.GabrielD@gmail.com>
2019-07-29 12:12:07 -0700

    Adding files
    Initial commit of various python scripts for dealing with audio features and plotting. Implementations are fairly naive and hack-y, still need some cleanup

---
+++ violin.py
@@ -0,0 +1,25 @@
+##Uses pyAudioANalysis to extract featrues and currently creates stacked violin plots
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import audioFeatureExtraction
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import seaborn as sns
+
+#Set features to extract from files, see documentaion for feature extraction
+#in pyAudioAnalysis. Ex. 0 = zero crossing rate, 1 = energy, etc.
+feature1 = 0
+feature2 = 0
+#For use later to push features into arrays for plotting, not implemented
+F1 = np.array([])
+F2 = np.array([])
+#Hard coded, assuming that all data files are in ./wavs and are all wav files
+#Extra files like .DS_Store on MacOS or other extraneous files break the loop
+for file in os.listdir("./wavs/"):
+    fname = os.fsdecode(file)
+    [Fs, x] = audioBasicIO.readAudioFile("./wavs/" + fname)
+    F, f_names = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
+    sns.set(style="whitegrid")
+    vplot = sns.violinplot(F[feature1,:], orient="v")
+
+plt.show()
---
+++ zcr.py
@@ -0,0 +1,16 @@
+##Uses pyAudioAnalysis to extract and plot ZCR
+from pyAudioAnalysis import audioBasicIO
+from pyAudioAnalysis import audioFeatureExtraction
+import matplotlib.pyplot as plt
+#Hard coded for one file, need to add loop or argument handling to specify data location
+[Fs, x] = audioBasicIO.readAudioFile("seven.wav")
+F, f_names = audioFeatureExtraction.stFeatureExtraction(x, Fs, 0.050*Fs, 0.025*Fs)
+plt.subplot(2,1,1);
+##F[0] is the first feature in the vector - ZCR
+plt.plot(F[0,:]);
+plt.xlabel('Frame no');
+plt.ylabel(f_names[0]);
+plt.subplot(2,1,2);
+norm_data = (x -x.mean())/x.std()
+plt.plot(x)
+plt.show()
