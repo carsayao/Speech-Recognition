e0b42c7506da4ea38e639c8d6c8d48c985b7d52f
Carlos Miguel Sayao <carsayao@pdx.edu>
2019-08-07 01:41:26 -0700

    calculate mean mt features with plot_features.py

--- plot_features.py
+++ plot_features.py
@@ -37,24 +37,25 @@ for folder in inputFolders:
     # Instantiate numpy array
     summed = np.zeros((68, 1))
 
-    print(folder.name)
+    print("/"+folder.name)
+    print("\t"+"Processing...")
     
     # Number of samples in folder
     samples = 0
     for f in os.scandir(folder):
-        #
-        if "summed" in f.name:
+        # Skip over
+        if "mean" in f.name:
             continue
-        print("\t"+f.name)
         
         # Import mt csv, reshape for compatibility in array operations
         importmt = np.loadtxt(outputDir+"/"+folder.name+"/"+f.name, delimiter=",", skiprows=1)
         importmt = importmt.reshape((68,1))
         summed = summed + importmt
         samples = samples + 1
+        print("\t\t"+f.name)
     
     summed = summed / samples
     # Save to folder name
     np.savetxt(outputDir+"/"+folder.name+"/"+folder.name+"_"+"mean"+"_mtfeatures.csv", summed)
-    print ("Successfully saved %s " % folder.name+"_"+"mean"+"_mtfeatures.csv")
+    print ("Successfully saved %s " % folder.name+"_"+"mean"+"_mtfeatures.csv\n")
 
